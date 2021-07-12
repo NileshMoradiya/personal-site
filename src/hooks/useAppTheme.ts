5a20b8ef51ebca7bc40c0dbdab947318f8f8e3a6
Nilesh <nilesh@imriel.com>
2021-07-12 12:35:50 +0530

    Cleared extra files

--- src/hooks/useAppTheme.ts
+++
@@ -1,9 +0,0 @@
-import { Theme, createTheme } from '@material-ui/core/styles';
-
-import defaultTheme from '../data/theme';
-import { useState } from 'react';
-
-export default function useAppTheme(): { theme: Theme } {
-    const [theme, setTheme] = useState<Theme>(createTheme(defaultTheme));
-    return { theme: theme };
-}
