2b1bc9517b09a1b5e27a615b59d04f8b06cd862f
Automated <actions@users.noreply.github.com>
2021-10-09 05:35:48 +0000

    Updates

--- static/js/9.0625dacf.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppersonal-site"]=this["webpackJsonppersonal-site"]||[]).push([[9],{452:function(e,t,c){"use strict";c.r(t);var s=c(90),a=c(213),r=c(78),n=c(167),i=(c(0),c(1));t.default=function(e){Object(s.a)(e);var t="Contact";return Object(i.jsx)(i.Fragment,{children:Object(i.jsx)(n.a,{title:t,description:t,children:Object(i.jsx)(r.b,{header:t,subHeader:"Places where you can reach out to me",children:Object(i.jsx)(a.a,{})})})})}}}]);
-//# sourceMappingURL=9.0625dacf.chunk.js.map
